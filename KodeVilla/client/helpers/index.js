22f673088675ea5cc70a35560c4994a3fbaad6f2
Owohlet <ike.owoh@meltwater.org>
2015-03-18 00:21:32 +0000

    subscribe button working well

---
+++ KodeVilla/client/helpers/index.js
@@ -0,0 +1,15 @@
+Template.index.events({
+	'click #submit': function () {
+		alert("Thanks for subscribing to KodeVilla, once we launch you will be the first to know!");
+
+	},
+	'click #subscribe1': function(){
+		window.scrollBy(0, -1000);
+	},
+	'click #subscribe2': function(){
+		window.scrollBy(0, -2000);
+	},
+	'click #subscribe3': function(){
+		window.scrollBy(0, -2300);
+	}
+});
